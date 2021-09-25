e7f9dc39bc3cac8805fe0a55e29e75701630eb5f
roberto3003 <roberto3003@riseup.net>
2021-09-25 15:04:37 +0700

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-fn largerst(list: &[i32]) -> i32 {
+// fn largerst(list: &[i32]) -> i32 {
+fn largerst<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
         if item > largest {
             largest = item;
  }
-    }
     largest
 }
+
 fn main() {
     let number_list= vec![34, 50, 100, 65];
 
