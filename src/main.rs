c1472887e7395273afc6afef27f7c506ed35dd0a
roberto3003 <roberto3003@riseup.net>
2021-09-25 13:14:26 +0700

    function largest number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 100, 65];
+
+    let mut largerst = number_list[0];
+
+    for number in number_list {
+        if number > largerst {
+            largerst = number;
+        }
+    }
+    println!("The largest number is {}", largerst);
 }
