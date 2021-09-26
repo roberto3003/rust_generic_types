d2949bcd2794277b83c555241e217121065f03ff
bobby3003 <roberto3003@riseup.net>
2021-09-26 15:37:11 +0700

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 // fn largerst(list: &[i32]) -> i32 {
-fn largerst<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
         if item > largest {
             largest = item;
         }
-        largest
     }
+
+    largest
 }
 
 fn main() {
     let number_list = vec![34, 50, 100, 65];
 
-    let result = largerst(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largerst(&number_list);
+    let result = largest(&number_list);
 
     println!("The largest number is {}", result);
 }
